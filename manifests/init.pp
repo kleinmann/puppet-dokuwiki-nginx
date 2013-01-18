e1cabe8a6051edc1b06caabb11232939224c84f0
Uwe Kleinmann <u.kleinmann@googlemail.com>
2013-01-18 13:10:21 +0100

    Preseed debconf to prevent stalling
    Even when you install this non-interactively, this blocks the
    installation. To prevent this, we preseed our debconf settings.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,23 @@
-class dokuwiki( $domain ) {
+class dokuwiki( $domain, $wiki_email = 'root@${::fqdn}', $wiki_title = 'Wiki' ) {
   package { 'dokuwiki':
-    name   => 'dokuwiki',
-    ensure => present,
+    name    => 'dokuwiki',
+    ensure  => present,
+    require => Exec['preseed debconf'],
+  }
+
+  file { 'debconf-settings-dokuwiki':
+    ensure  => 'present',
+    path    => '/tmp/debconf-settings-dokuwiki.cfg',
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0600,
+    content => template('dokuwiki/debconf.erb'),
+  }
+
+  exec { 'preseed debconf':
+    command => 'debconf-set-selections /tmp/debconf-settings-dokuwiki.cfg',
+    require => File['debconf-settings-dokuwiki'],
+    path    => '/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin'
   }
 
   nginx::resource::vhost { $domain:
