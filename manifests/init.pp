0cea57c57c610220a6e0a72fe185040ef1a11b13
Uwe Kleinmann <u.kleinmann@googlemail.com>
2013-01-19 12:28:06 +0100

    Try packaged dokuwiki again with plain source.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class dokuwiki( $domain, $wiki_email = 'root@${::fqdn}', $wiki_title = 'Wiki' ) 
     ensure   => present,
     name     => 'dokuwiki',
     provider => 'dpkg',
-    source   => '/opt/debs/dokuwiki_0.0.20120125b-1_all.deb',
+    source   => '/opt/debs/dokuwiki_0.0.20121013_all.deb',
     require  => File['dokuwiki-package'],
   }
 
@@ -13,11 +13,11 @@ class dokuwiki( $domain, $wiki_email = 'root@${::fqdn}', $wiki_title = 'Wiki' ) 
 
   file { 'dokuwiki-package':
     ensure  => 'present',
-    path    => '/opt/debs/dokuwiki_0.0.20120125b-1_all.deb',
+    path    => '/opt/debs/dokuwiki_0.0.20121013_all.deb',
     owner   => 'root',
     group   => 'root',
     mode    => 0644,
-    source  => 'puppet:///modules/dokuwiki/dokuwiki_0.0.20120125b-1_all.deb',
+    source  => 'puppet:///modules/dokuwiki/dokuwiki_0.0.20121013_all.deb',
     require => File['/opt/debs'],
   }
 
@@ -32,33 +32,26 @@ class dokuwiki( $domain, $wiki_email = 'root@${::fqdn}', $wiki_title = 'Wiki' ) 
 
   file { 'dokuwiki-local-configuration':
     ensure  => 'present',
-    path    => '/etc/dokuwiki/local.php',
+    path    => '/var/www/dokuwiki/conf/local.php',
     owner   => 'root',
     group   => 'root',
     mode    => 0644,
     content => template('dokuwiki/local.php.erb'),
   }
-  file { 'dokuwiki-acl':
+  file { 'dokuwiki-users':
     ensure  => 'present',
-    path    => '/var/lib/dokuwiki/acl/users.auth.php',
+    path    => '/var/www/dokuwiki/conf/users.auth.php',
     owner   => 'root',
     group   => 'root',
     mode    => 0600,
     content => template('dokuwiki/users.auth.php.erb'),
   }
-
-  # Create symlinks
-  file { '/etc/dokuwiki/users.auth.php':
-   ensure  => 'link',
-   target  => '/var/lib/dokuwiki/acl/users.auth.php',
-   require => File['dokuwiki-acl'],
-  }
-  file { '/etc/dokuwiki/acl.auth.php':
-   ensure => 'link',
-   target => '/var/lib/dokuwiki/acl/acl.auth.php',
-  }
-  file { '/var/lib/dokuwiki/inc':
-   ensure => 'link',
-   target => '/usr/share/dokuwiki/inc',
+  file { 'dokuwiki-acl':
+    ensure  => 'present',
+    path    => '/var/www/dokuwiki/conf/acl.auth.php',
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0600,
+    content => template('dokuwiki/acl.auth.php.erb'),
   }
 }
