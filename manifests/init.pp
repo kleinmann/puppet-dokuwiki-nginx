13133db546066dc6e20f1de1c5218b501c274d9a
Uwe Kleinmann <u.kleinmann@googlemail.com>
2013-01-19 11:59:19 +0100

    Switch to plain deb based installation.
    The ubuntu dokuwiki package installs apache2, which I don't need.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,24 @@
 class dokuwiki( $domain, $wiki_email = 'root@${::fqdn}', $wiki_title = 'Wiki' ) {
   package { 'dokuwiki':
-    name    => 'dokuwiki',
-    ensure  => present,
-    require => Exec['preseed debconf'],
+    ensure   => present,
+    name     => 'dokuwiki',
+    provider => 'dpkg',
+    source   => '/opt/debs/dokuwiki_0.0.20120125b-1_all.deb',
+    require  => File['dokuwiki-package'],
   }
 
-  file { 'debconf-settings-dokuwiki':
+  file { '/opt/debs':
+    ensure => 'directory',
+  }
+
+  file { 'dokuwiki-package':
     ensure  => 'present',
-    path    => '/tmp/debconf-settings-dokuwiki.cfg',
+    path    => '/opt/debs/dokuwiki_0.0.20120125b-1_all.deb',
     owner   => 'root',
     group   => 'root',
-    mode    => 0600,
-    content => template('dokuwiki/debconf.erb'),
-  }
-  exec { 'preseed debconf':
-    command => 'debconf-set-selections /tmp/debconf-settings-dokuwiki.cfg',
-    require => File['debconf-settings-dokuwiki'],
-    path    => '/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin'
+    mode    => 0644,
+    source  => 'puppet:///modules/dokuwiki/dokuwiki_0.0.20120125b-1_all.deb',
+    require => File['/opt/debs'],
   }
 
   file { 'dokuwiki-nginx-vhost':
@@ -26,5 +28,37 @@ class dokuwiki( $domain, $wiki_email = 'root@${::fqdn}', $wiki_title = 'Wiki' ) 
     group   => 'root',
     mode    => 0600,
     content => template('dokuwiki/nginx_vhost.erb'),
-  }  
+  }
+
+  file { 'dokuwiki-local-configuration':
+    ensure  => 'present',
+    path    => '/etc/dokuwiki/local.php',
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+    content => template('dokuwiki/local.php.erb'),
+  }
+  file { 'dokuwiki-acl':
+    ensure  => 'present',
+    path    => '/var/lib/dokuwiki/acl/users.auth.php',
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0600,
+    content => template('dokuwiki/users.auth.php.erb'),
+  }
+
+  # Create symlinks
+  file { '/etc/dokuwiki/users.auth.php':
+   ensure  => 'link',
+   target  => '/var/lib/dokuwiki/acl/users.auth.php',
+   require => File['dokuwiki-acl'],
+  }
+  file { '/etc/dokuwiki/acl.auth.php':
+   ensure => 'link',
+   target => '/var/lib/dokuwiki/acl/acl.auth.php',
+  }
+  file { '/var/lib/dokuwiki/inc':
+   ensure => 'link',
+   target => '/usr/share/dokuwiki/inc',
+  }
 }
